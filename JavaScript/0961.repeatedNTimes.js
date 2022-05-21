187dd9ead4869bfc7ca8dd63319648145e302d04
zyuandong <de1719@163.com>
2022-05-21 23:43:30 +0800

    add: 961

---
+++ JavaScript/0961.repeatedNTimes.js
@@ -0,0 +1,21 @@
+/*
+ * @lc app=leetcode.cn id=961 lang=javascript
+ *
+ * [961] 在长度 2N 的数组中找出重复 N 次的元素
+ */
+
+// @lc code=start
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var repeatedNTimes = function (nums) {
+  let set = new Set();
+  for (let i = 0; i < nums.length; i++) {
+    if (set.has(nums[i])) {
+      return nums[i];
+    }
+    set.add(nums[i]);
+  }
+};
+// @lc code=end
