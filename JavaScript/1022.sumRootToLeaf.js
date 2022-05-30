7a41e3e1ea0c4f6e8c6813c0068af701051b44ca
zyuandong <de1719@163.com>
2022-05-30 23:00:53 +0800

    add: 1022

---
+++ JavaScript/1022.sumRootToLeaf.js
@@ -0,0 +1,38 @@
+/*
+ * @lc app=leetcode.cn id=1022 lang=javascript
+ *
+ * [1022] 从根到叶的二进制数之和
+ */
+
+// @lc code=start
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {number}
+ */
+var sumRootToLeaf = function (root) {
+  const dfs = (root, val) => {
+    if (!root) {
+      return 0;
+    }
+
+    val = (val << 1) | root.val;
+    if (!root.left && !root.right) {
+      return val;
+    }
+
+    return dfs(root.left, val) + dfs(root.right, val);
+  };
+
+  return dfs(root, 0);
+};
+
+// const addStr = (str, item) => `${str}${item}`;
+// @lc code=end
