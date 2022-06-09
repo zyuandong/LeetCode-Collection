81625b1323d6fc5892bb8b2cf086e89925556e4e
zyuandong <de1719@163.com>
2022-06-09 09:04:08 +0800

    add: 1037

---
+++ JavaScript/1037.isBoomerang.js
@@ -0,0 +1,17 @@
+/*
+ * @lc app=leetcode.cn id=1037 lang=javascript
+ *
+ * [1037] 有效的回旋镖
+ */
+
+// @lc code=start
+/**
+ * @param {number[][]} points
+ * @return {boolean}
+ */
+var isBoomerang = function (points) {
+  const v1 = [points[1][0] - points[0][0], points[1][1] - points[0][1]];
+  const v2 = [points[2][0] - points[0][0], points[2][1] - points[0][1]];
+  return v1[0] * v2[1] - v1[1] * v2[0] != 0;
+};
+// @lc code=end
